10cd7d3f097b95ece2e4bcdc1fd83c15f02210f4
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2021-02-28 13:22:15 -0300

    configurando o spawn

--- Assets/Scripts/SpawnControl.cs
+++ Assets/Scripts/SpawnControl.cs
@@ -3,76 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class SpawnControl : NetworkBehaviour
+public class SpawnControl : MonoBehaviour
 {
-    //public GameObject spawnLocate;
-    public GameObject gemaPrefabe;
+    GameObject spawn1;
+    GameObject spawn2;
 
-    public bool instanciado;
-    public GameObject instancia;
-
-    Vector3 spawnPosition = new Vector3(0, 3, 0);
-
-    int pos, posaux;
-    public override void OnStartServer()
+    public void SetPosition(PlayerMove player, bool isClient)
     {
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        instanciado = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("l"))
+        if (isClient)
         {
-            Destroy(instancia);
-            instanciado = false;
-        }
 
-        if (!instanciado)
+        }
+        else
         {
-            posaux = pos;
-            while(pos == posaux)
-            {
-                pos = Random.Range(1, 6);
-            }
 
-            //Debug.Log("Var pos: " + pos);
-            switch (pos)
-            {
-                case 1:
-                    spawnPosition.x = 82;
-                    spawnPosition.z = 350;
-                    break;
-
-                case 2:
-                    spawnPosition.x = 198;
-                    spawnPosition.z = 8;
-                    break;
-                case 3:
-                    spawnPosition.x = 270;
-                    spawnPosition.z = 161;
-                    break;
-                case 4:
-                    spawnPosition.x = 395;
-                    spawnPosition.z = 131;
-                    break;
-                case 5:
-                    spawnPosition.x = 390;
-                    spawnPosition.z = 360;
-                    break;
-
-            }
-            
-            instancia = Instantiate(gemaPrefabe, spawnPosition, this.transform.rotation);
-
-            instanciado = true;
         }
-
-        
     }
+
+
 }
